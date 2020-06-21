0a26a1f997eecfe2790ea28f21167e816487cdda
Carlos Breno Pereira Silva <breno.silva@ifrn.edu.br>
2020-06-20 21:38:00 -0300

    Refactoring

--- scripts/nodejs/release.sh
+++ scripts/nodejs/release.sh
@@ -2,7 +2,7 @@ CURRENT_DIR=$(pwd)
 BUILD_DIR="/tmp/slothy"
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
-cp slothy/api/frontend/static/slothy.js /tmp/slothy
+cp slothy/test/frontend/static/js/slothy.js /tmp/slothy
 cp scripts/nodejs/package.json /tmp/slothy
 cd $BUILD_DIR
 npm publish --access public
--- scripts/pypi/setup.py
+++ scripts/pypi/setup.py
@@ -5,9 +5,9 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='slothy',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
-    install_requires=['Django', 'django_rest_framework', 'six'],
+    install_requires=['Django', 'django_rest_framework', 'six', 'django-cors-headers'],
     include_package_data=True,
     license='BSD License',
     description='Slothy framework',
