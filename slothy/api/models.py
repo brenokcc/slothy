fa895baf3fbe4f633b3eb6c47cbede8914e2a809
Carlos Breno Pereira Silva <breno.silva@ifrn.edu.br>
2021-01-26 08:10:37 -0300

    Refatoração

---
+++ slothy/api/models.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import six
+from django.apps import apps
+from slothy.db import models
+from django.contrib.auth import base_user
+
+
+class Group(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=255)
+    lookup = models.CharField(verbose_name='Chave', max_length=255)
+
+    class Meta:
+        verbose_name = 'Grupo'
+        verbose_name_plural = 'Grupos'
+
+    def get_users(self):
+        from django.conf import settings
+        user_model_name = settings.AUTH_USER_MODEL.split('.')[1].lower()
+        return getattr(self, '{}_set'.format(user_model_name)).all()
+
+    def __str__(self):
+        return self.name
+
+
+class AbstractUser(six.with_metaclass(models.ModelBase, base_user.AbstractBaseUser, models.Model)):
+
+    password = models.CharField(verbose_name='Senha', null=True, blank=True, default='!', max_length=255)
+    last_login = models.DateTimeField(verbose_name='Último Login', null=True, blank=True)
+    groups = models.ManyToManyField(Group, verbose_name='Grupos', blank=True)
+
+    class Meta:
+        abstract = True
+
+    def change_password(self, raw_password):
+        super().set_password(raw_password)
+        super().save()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        token_model = apps.get_model('authtoken', 'Token')
+        token_model.objects.get_or_create(user=self)
